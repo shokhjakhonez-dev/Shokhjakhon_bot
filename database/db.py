ca1853a05500f60750dcf5739c01ca1022da85dd
shokhjakhonez-dev <shokhjakhon.e.z@icloud.com>
2026-07-24 16:13:38 +0500

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,1 +1,52 @@
-# temp
+import aiosqlite
+
+DB_NAME = "lab.db"
+
+async def create_tables():
+    async with aiosqlite.connect(DB_NAME) as db:
+
+        # Doktorlar
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS doctors (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            fullname TEXT,
+            phone TEXT,
+            clinic TEXT
+        )
+        """)
+
+        # Shogirtlar
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS students (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            fullname TEXT,
+            phone TEXT
+        )
+        """)
+
+        # Ishlar
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS jobs (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            doctor_id INTEGER,
+            patient TEXT,
+            job_type TEXT,
+            price INTEGER,
+            paid INTEGER DEFAULT 0,
+            debt INTEGER DEFAULT 0,
+            status TEXT,
+            created_at TEXT
+        )
+        """)
+
+        # To'lovlar
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS payments (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            job_id INTEGER,
+            amount INTEGER,
+            payment_date TEXT
+        )
+        """)
+
+        await db.commit()
